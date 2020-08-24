31c08a42d98279f1636ce424098c6637069622f9
matthewproskils <65041704+matthewproskils@users.noreply.github.com>
2020-08-23 22:35:09 -0700

    Update and rename new/Interpreter.js to src/Interpreter.js

--- src/Interpreter.js
+++ src/Interpreter.js
@@ -1,12 +1,20 @@
 const builtins = require("./Builtins.js")
 const Scope = require("./Scope.js")
 
+function Unique(myArray) {
+	return myArray.length === new Set(myArray).size;
+}
+
 function interpret(ast, scope) {
 	if (ast === undefined) return
-
 	if (ast.type === "Program") {
-		let globalScope = new Scope(builtins, null)
-		return ast.body.map(k => interpret(k, globalScope))
+		//Check If Body Is Unefined
+		if (ast.body[0] == null) {
+			return
+		} else {
+			let globalScope = new Scope(builtins, null)
+			return ast.body.map(k => interpret(k, globalScope))
+		}
 	}
 
 	if (ast.type === 'NullLiteral') {
@@ -21,16 +29,33 @@ function interpret(ast, scope) {
 
 	if (ast.type === 'Identifier') {
 		const { name } = ast;
-
-		return scope.get(name, scope);
+		let got = scope.get(name, scope);
+		if (got == void (0)) {
+			throw new Error(`${ast.name} is undefined`)
+		}
+		if (ast.other != []) {
+			ast.other.forEach(el => {
+				if (el[0] == ".") {
+					if (typeof (got) != 'object' && !Array.isArray(got)) {
+						throw new Error(`${got} is not an object`)
+					}
+					got = got[el[1].join('')]
+				} else if (el[0] == '[') {
+					console.log()
+					if (!Array.isArray(got)) {
+						throw new Error(`${got} is not an array`)
+					}
+					got = got[parseInt(el[1].join(''))]
+				}
+			})
+		}
+		return got
 	}
 
 	if (ast.type === "CallExpr") {
 		const { id: callee, args } = ast
-
 		const argVals = ast.args.map(arg => interpret(arg, scope))
 		const closureOrFunc = interpret(callee, scope);
-
 		switch (closureOrFunc.type) {
 			case 'function':
 				return closureOrFunc.call.apply(null, argVals);
@@ -72,24 +97,44 @@ function interpret(ast, scope) {
 
 		let containsFun = scope.get(name);
 
-		if (containsFun) throw new Error(`variable ${name} is redeclared`)
+		if (containsFun) throw new Error(`Function ${name} is redeclared`)
 
 		scope.set(name, funBody);
 
 		return undefined
 	}
 
+	if (ast.type == 'ArrayExpr') {
+		let returnarray = []
+		ast.value.forEach(el => returnarray.push(interpret(el, scope)))
+		return returnarray
+	}
+
 	if (ast.type === "Assignment") {
-		const { id: { name }, expr } = ast
+		let { id: { name }, expr } = ast
 
 		const exprVal = interpret(expr, scope)
 
 		let containsVar = scope.get(name);
 
-		if (!containsVar) throw new Error(`variable ${name} is not declared`)
+		if (!containsVar) throw new Error(`Variable ${name} is not declared yet`)
 
-		scope.set(name, exprVal)
-
+		if (ast.other != []) {
+			let joinedfull = []
+			ast.id.other.forEach(el => {
+				let joined = []
+				el.forEach(al => {
+					if (Array.isArray(al)) {
+						joined.push(al.join(''))
+					}
+				})
+				joinedfull.push(joined.join(''))
+			})
+			let x = scope.scope[name]
+			scope.objset(name, joinedfull, exprVal)
+		} else {
+			scope.set(name, exprVal)
+		}
 		return undefined
 	}
 
@@ -149,6 +194,46 @@ function interpret(ast, scope) {
 		return interpret(test, scope) ? interpret(consequent, scope) : interpret(alternate, scope)
 	}
 
+	if (ast.type == 'Object') {
+		let matches = []
+		ast.value.forEach(el => matches.push(el[0].value))
+		if (Unique(matches)) {
+			let total = {}
+			ast.value.forEach(el => {
+				total[interpret(el[0])] = interpret(el[1])
+			})
+			return total
+		} else {
+			throw new Error(`Name Repeats Are Not Allowed In Object`)
+		}
+	}
+
+	if (ast.type == 'loop') {
+		let { start, end, skip: step, variable } = ast
+		start = parseInt(start)
+		end = parseInt(end)
+		step = parseInt(step)
+
+		for (let x = start; x <= end; x = x + skip) {
+			let otherscope = new Scope({}, scope)
+			otherscope.set(ast.variable.toString(), x)
+			ast.code.forEach(el => { interpret(el, otherscope) })
+		}
+		return
+	}
+
+	if (ast.type == 'if') {
+		if (eval(interpret(ast.check.one, scope) + ast.check.sign + interpret(ast.check.two, scope))) {
+			ast.iftrue.forEach(el => interpret(el, scope))
+		} else {
+			ast.iffalse.forEach(el => interpret(el, scope))
+		}
+		return
+	}
+
+	if (ast.type == 'comment') {
+		return
+	}
 
 	throw new Error(`unknown type: ${ast.type}`)
 }
